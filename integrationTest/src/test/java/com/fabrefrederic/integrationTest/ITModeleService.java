83a5f3af6217b0c84605304d603ac68e38b3198f
Fred <fabrefrederic@free.fr>
2014-12-01 13:14:44 +0100

    - Add a first it test, its displays a message in maven logs - Modify the profile name

---
+++ integrationTest/src/test/java/com/fabrefrederic/integrationTest/ITModeleService.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.fabrefrederic.integrationTest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author fred
+ *
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+public class ITModeleService {
+	
+	@Test
+	public void test() {
+		System.out.println("toto");
+	}
+}
